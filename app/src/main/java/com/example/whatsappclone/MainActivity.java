070d210ba6056a3cc0a12b67bc33d7d2568c8d2c
Divyansh Thakur <divyansh2140@gmail.com>
2019-06-29 12:41:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.whatsappclone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.ParseInstallation;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // Save the current Installation to Back4App
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+    }
+}
